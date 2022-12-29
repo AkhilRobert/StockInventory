97d9347cc8952c23f349edd604a352d71e37b403
Akhil Robert <akhilrobert2412@gmail.com>
2022-12-29 11:01:20 +0530

    Added prisma user schema

---
+++ prisma/migrations/20221229052656_created_user/migration.sql
@@ -0,0 +1,13 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('HOD', 'SUPERINTENDENT', 'STAFF');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "role" "Role" NOT NULL,
+    "username" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
