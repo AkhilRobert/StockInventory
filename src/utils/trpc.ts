3f1e98cec4ad2dc29c5a0690a6459d080bf7e6e2
Akhil Robert <akhilrobert2412@gmail.com>
2023-01-01 11:09:06 +0530

    Added global auth error handler

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -1,7 +1,8 @@
-import { httpBatchLink } from "@trpc/client";
+import { httpBatchLink, TRPCClientError } from "@trpc/client";
 import { createTRPCNext } from "@trpc/next";
 import SuperJSON from "superjson";
 import type { AppRouter } from "../server/_app";
+import Router from "next/router";
 
 const getBaseUrl = () => {
   if (typeof window !== "undefined") return "";
@@ -30,7 +31,20 @@ export const trpc = createTRPCNext<AppRouter>({
       /**
        * @link https://tanstack.com/query/v4/docs/reference/QueryClient
        **/
-      // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
+      queryClientConfig: {
+        defaultOptions: {
+          queries: {
+            staleTime: 60,
+            retry: (count, err) => {
+              if (err instanceof TRPCClientError) {
+                if (err.data.code === "UNAUTHORIZED") Router.replace("/");
+              }
+
+              return count < 2;
+            },
+          },
+        },
+      },
     };
   },
   /**
