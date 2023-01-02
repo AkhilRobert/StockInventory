61fafa2d8a52444fdd5d7f4080bafba96767e359
Akhil Robert <akhilrobert2412@gmail.com>
2023-01-02 08:41:41 +0530

    Added react query devtools

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import type { AppProps } from "next/app";
 import { trpc } from "../utils/trpc";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default trpc.withTRPC(App);
