cfbf6eb45dc2d917c55b6ac2af2f45a0be4dc15a
Akhil Robert <akhilrobert2412@gmail.com>
2023-01-03 08:38:46 +0530

    added mantine to the project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import type { AppProps } from "next/app";
 import { trpc } from "../utils/trpc";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { MantineProvider } from "@mantine/core";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <ReactQueryDevtools initialIsOpen={false} />
-      <Component {...pageProps} />
+      <MantineProvider withGlobalStyles withNormalizeCSS>
+        <Component {...pageProps} />
+      </MantineProvider>
     </>
   );
 };
