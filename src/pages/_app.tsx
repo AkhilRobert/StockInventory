2cd2425046b5f1f3f0c345aca7259f30b53c5088
Akhil Robert <akhilrobert2412@gmail.com>
2023-02-23 11:02:46 +0530

    Added progress bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,14 @@ import { trpc } from "../utils/trpc";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { MantineProvider } from "@mantine/core";
 import { NotificationsProvider } from "@mantine/notifications";
+import NextNProgress from "nextjs-progressbar";
 import "../../public/global.css";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <ReactQueryDevtools initialIsOpen={false} />
+      <NextNProgress />
       <MantineProvider withGlobalStyles withNormalizeCSS>
         <NotificationsProvider zIndex={5000}>
           <Component {...pageProps} />
